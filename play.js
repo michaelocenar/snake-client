ed1ff2dcfb2365889a46b96cd7af17f668a7b515
viacaelestis <affectuminflamma@gmail.com>
2023-02-15 06:14:10 -0700

    enabled client to exit with ctrl+c

--- play.js
+++ play.js
@@ -8,5 +8,25 @@ const conn = net.createConnection({
   port: 50541
 });
 
+const handleUserInput = function (data) {
+  if (data === '\u0003') {
+    process.exit();
+  }
+};
+
+process.stdin.on("data", handleUserInput);
+
+// setup interface to handle user input from stdin
+
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  stdin.on("data", handleUserInput);
+  return stdin;
+};
+
 // pass the conn object to the connect function
 connect(conn);
+setupInput();
